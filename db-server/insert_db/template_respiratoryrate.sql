94adbe5b8e2cdcb964b4719956167d1a6a5ea0f0
HuyNguyen1810 <130727505+HuyNguyen1810@users.noreply.github.com>
2023-11-30 00:05:33 +0700

    Update template_respiratoryrate.sql

--- db-server/insert_db/template_respiratoryrate.sql
+++ db-server/insert_db/template_respiratoryrate.sql
@@ -1,7 +1,7 @@
 INSERT INTO
     QUARATINE_CAMP_DB.RespiratoryRate (
-        RespiratoryRate.TestID,
-        RespiratoryRate.NumberofBreathPerMinute
+        RespiratoryRate_Test_ID,
+        RespiratoryRate_Number_of_breath_per_minute
     )
 VALUES
     (
