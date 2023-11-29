383d76c7b7aaa1917f723a610ffe995cde801976
HuyNguyen1810 <130727505+HuyNguyen1810@users.noreply.github.com>
2023-11-30 00:01:13 +0700

    Create template_pcrtest.sql

---
+++ db-server/insert_db/template_pcrtest.sql
@@ -0,0 +1,12 @@
+INSERT INTO
+    QUARATINE_CAMP_DB.PCRTest (
+        PCRTest_TestID,
+        PCRTest_CTValue,
+        PCRTest_Result
+    )
+VALUES
+    (
+        %s,
+        %s,
+        %s
+    )
