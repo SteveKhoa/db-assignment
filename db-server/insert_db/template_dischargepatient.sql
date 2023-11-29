f3d3becc190b36b2cb4762603bc2e87383d42dcc
HuyNguyen1810 <130727505+HuyNguyen1810@users.noreply.github.com>
2023-11-30 00:11:53 +0700

    Create template_dischargepatient.sql

---
+++ db-server/insert_db/template_dischargepatient.sql
@@ -0,0 +1,10 @@
+INSERT INTO
+    QUARATINE_CAMP_DB.DischargePatient (
+        DischargePatient_PatientID,
+        DischargePatient_Discharge_Date
+    )
+VALUES
+    (
+        %s,
+        %s
+    )
