954a17d41bfb54e5cf09dc83a76e70fa8b77bb4b
HuyNguyen1810 <130727505+HuyNguyen1810@users.noreply.github.com>
2023-11-30 00:13:36 +0700

    Create template_symptoms.sql

---
+++ db-server/insert_db/template_symptoms.sql
@@ -0,0 +1,12 @@
+INSERT INTO
+    QUARATINE_CAMP_DB.Symptoms (
+        Symptoms_Time,
+        Symptoms_Patient_ID,
+        Symptoms_Symptoms
+    )
+VALUES
+    (
+        %s,
+        %s,
+        %s
+    )
