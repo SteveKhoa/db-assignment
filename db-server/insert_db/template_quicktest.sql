f18eb38d567c8b61e9bb173af93dca0ca4ac2b6a
HuyNguyen1810 <130727505+HuyNguyen1810@users.noreply.github.com>
2023-11-30 00:08:36 +0700

    Create template_quicktest.sql

---
+++ db-server/insert_db/template_quicktest.sql
@@ -0,0 +1,12 @@
+INSERT INTO
+    QUARATINE_CAMP_DB.QuickTest (
+        QuickTest_TestID,
+        QuickTest_CT_Value,
+        QuickTest_Result
+    )
+VALUES
+    (
+        %s,
+        %s,
+        %s
+    )
