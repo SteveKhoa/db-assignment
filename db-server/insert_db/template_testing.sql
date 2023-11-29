d4b532d80ec6b8873163f55ed8bf16e1c9b35061
HuyNguyen1810 <130727505+HuyNguyen1810@users.noreply.github.com>
2023-11-29 23:57:50 +0700

    Create template_testing.sql

---
+++ db-server/insert_db/template_testing.sql
@@ -0,0 +1,16 @@
+INSERT INTO
+    QUARATINE_CAMP_DB.Testing(
+        Testing_patientID,
+        Testing_TestID,
+        Testing_StaffID,
+        Testing_Date
+    )
+VALUE
+    (
+       %s,
+       %s,
+       %s,
+       %s,
+       %s
+    )
+    
