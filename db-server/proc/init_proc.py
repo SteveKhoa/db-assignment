f7c17aea6eb34f2654f1d6134ca5a823c3bb3318
Khoa LTN <vietnamkhoale@gmail.com>
2023-12-07 10:05:25 +0700

    fix: cut out DELIMITER before python query

--- db-server/proc/init_proc.py
+++ db-server/proc/init_proc.py
@@ -16,23 +16,36 @@ def get_query_statement(filename):
 
 if __name__ == "__main__":
     conn = mysql.connector.connect(host="localhost", user="root")
-    cursor = conn.cursor()
+    cursor = conn.cursor()  
 
     procs_path = [
         "update-pcr.sql",
         "show-nva.sql",
         "calculate-test.sql",
-        "sort-nurse.sql",
+        "sort-nurse.sql"
     ]
 
     for proc_path in procs_path:
         query = get_query_statement(proc_path)
+
+        # Remove `DELIMITER` keyword since Python Connector does not support it
+        # It is needed on Client databases, however, to cope with delimiter problems in
+        # SQL procedure.
+        # https://stackoverflow.com/questions/75258442/using-mysql-connector-python-how-to-insert-a-complicated-with-delimiter-inst
+        query = query.replace('DELIMITER $$', '')
+        query = query.replace('DELIMITER ;', '')
+        query = query.replace('$$', '')
         
         try:
-            cursor.execute(query)
+            for exec in cursor.execute(query, multi=True):
+                # Because iterator is "lazily updated", must iterate
+                # the cursor in multi-query execution,
+                # otherwise, no query would execute.
+                pass
         except mysql.connector.Error as e:
-            print("SQL Error: {}", e)
-            print("\n\n")
+            print("===========================\n")
+            print("SQL Error: \n", e)
+            print("===========================\n\n")
         
         conn.commit()
     
