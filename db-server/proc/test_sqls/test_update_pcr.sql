1e13b00e383793ad289bba4d41cb5f178d6b3d3f
Khoa LTN <vietnamkhoale@gmail.com>
2023-12-15 09:31:56 +0700

    feat: testcase data for test_update_pcr.sql

--- db-server/proc/test_sqls/test_update_pcr.sql
+++ db-server/proc/test_sqls/test_update_pcr.sql
@@ -1,3 +1,18 @@
+INSERT INTO quaratine_camp_db.Testing values ("P00000001", "T00000046", "E00000005", "2023-08-13");
+INSERT INTO quaratine_camp_db.Testing values ("P00000002", "T00000047", "E00000005", "2023-06-15");
+INSERT INTO quaratine_camp_db.Testing values ("P00000003", "T00000048", "E00000005", "2023-12-20");
+INSERT INTO quaratine_camp_db.Testing values ("P00000004", "T00000049", "E00000005", "2023-12-03");
+INSERT INTO quaratine_camp_db.Testing values ("P00000005", "T00000050", "E00000005", "2023-11-01");
+
+INSERT INTO quaratine_camp_db.PCRTest values ("T00000046", NULL, "Negative");
+INSERT INTO quaratine_camp_db.PCRTest values ("T00000047", NULL, "Negative");
+INSERT INTO quaratine_camp_db.PCRTest values ("T00000048", NULL, "Negative");
+INSERT INTO quaratine_camp_db.PCRTest values ("T00000049", NULL, "Negative");
+INSERT INTO quaratine_camp_db.PCRTest values ("T00000050", NULL, "Negative");
+
+
+
+
 SET SQL_SAFE_UPDATES = 0;
 
 CALL UpdatePCR("2022-1-1");
